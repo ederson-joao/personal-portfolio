40bc1b5d9b67e7df7b1b0d5143cb8f9eb35d8739
Ederson João <ederson.joao@outlook.com>
2024-02-29 15:21:41 -0300

    feat: header creation and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import  Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <header>
+        <div className='containerHeader'>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic"></Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item  href="#sobre">Sobre</Dropdown.Item>
+            <Dropdown.Item  href="#skills">Habilidades</Dropdown.Item>
+            <Dropdown.Item  href="#projects">Projetos</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+          <h2>Portfólio</h2>
+          <label for="checkbox" className='colorTheme'>
+            <div className='colorThemePicker'></div>
+          </label>
+        </div>
+      </header>
   );
 }
 
